c331d7f10d6a9955badfeec6a4beb2647a8f732b
noocaaa <noeliacarrascovilar@gmail.com>
2023-10-13 23:11:49 +0200

    Separacion alerta requisitos

--- app/views/student_views.py
+++ app/views/student_views.py
@@ -186,8 +186,6 @@ def principal():
     for module_prog in modules_progress:
         module_id = module_prog['module'].id
 
-        print("fff", module_id)
-
         if module_id <= last_module_completely_done: 
             module_prog['available'] = True
 
@@ -311,11 +309,8 @@ def module_exercise(module_id):
     elif next_global_order_entry.content_type == "Exercises":
         exercise = get_exercise(next_global_order_entry.content_id)
         if exercise and exercise.module_id != module_id:
-            print("a")
             return redirect(url_for('student.module_exercise', module_id=exercise.module_id))
         if exercise:
-            print (module_id)
-            print ("b")
             return render_template('exercise.html', avatar_id=avatar_id, username=username, exercise=exercise, exercise_language=exercise.language)
 
     # Redirect to principal if no action is taken.
